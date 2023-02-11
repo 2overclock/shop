9d4f1b74a97d5e90a2b1c9f3b2a08a4fa5677b3c
Ilke <milosilijic@gmail.com>
2023-02-11 17:55:01 +0100

    Test inputs

--- shop.web/Areas/Admin/Controllers/DashboardController.cs
+++ shop.web/Areas/Admin/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using shop.web.Areas.Admin.Models;
 
 namespace shop.web.Areas.Admin.Controllers
 {
@@ -9,6 +11,36 @@ namespace shop.web.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Test1()
+        {
+            ViewBag.TestEnumValues = Enum.GetValues(typeof(TestEnum)).Cast<TestEnum>().Select(item => new SelectListItem
+            {
+                Text = item.ToString(),
+                Value = ((int)item).ToString()
+            });
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Test1(TestModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add To DB and return some page
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.TestEnumValues = Enum.GetValues(typeof(TestEnum)).Cast<TestEnum>().Select(item => new SelectListItem
+            {
+                Text = item.ToString(),
+                Value = ((int)item).ToString()
+            });
+
+            return View(model);
         }
     }
 }
---
+++ shop.web/Areas/Admin/Models/TestModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shop.web.Areas.Admin.Models
+{
+    public class TestModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public bool CheckMe { get; set; }
+
+        public List<TestEnum> CheckAllOfMe { get; set; }
+
+        public TestEnum TestMe { get; set; }
+
+        [Display(Name = "Select me")]
+        public TestEnum SelectMe { get; set; }
+    }
+
+    public enum TestEnum
+    {
+        Test1 = 1,
+        Test2 = 2,
+        Test3 = 3,
+        Test4 = 4
+    }
+}
